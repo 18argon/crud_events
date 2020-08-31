d9dfe92607bf91e9595492b90ebeabd9a86bdc41
Pabolo Pires <pabolo18@gmail.com>
2020-08-31 06:18:49 -0300

    Add .env

--- client/src/services/events-service.ts
+++ client/src/services/events-service.ts
@@ -2,21 +2,31 @@ import Event from '@/models/Event';
 
 export default {
   async fetchEvents(): Promise<Event[]> {
-    return fetch('http://localhost:3000/events')
+    return fetch(`${process.env.VUE_APP_API_URL}/events`)
       .then((response) => response.json());
   },
   async fetchEvent(id: string): Promise<Event> {
-    return fetch(`http://localhost:3000/events/${id}`)
+    return fetch(`${process.env.VUE_APP_API_URL}/events/${id}`)
       .then((response) => response.json());
   },
   async deleteEvent(eventId: string): Promise<void> {
-    fetch(`http://localhost:3000/events/${eventId}`, {
+    fetch(`${process.env.VUE_APP_API_URL}/events/${eventId}`, {
       method: 'DELETE',
+    });
+  },
+  async createEvent(event: Event): Promise<void> {
+    console.log(event);
+    fetch(`${process.env.VUE_APP_API_URL}/events`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(event),
     });
   },
   async updateEvent(event: Event): Promise<void> {
     console.log(event);
-    fetch(`http://localhost:3000/events/${event.id}`, {
+    fetch(`${process.env.VUE_APP_API_URL}/events/${event.id}`, {
       method: 'PUT',
       headers: {
         'content-type': 'application/json',
