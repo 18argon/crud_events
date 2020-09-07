3c2f9321e01e09bbadce9145cef4e94d65625ef2
Pabolo Pires <pabolo18@gmail.com>
2020-09-07 08:17:07 -0300

    Atualizando configurações do ESLint para funcionar em TypeScript files

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,12 +3,19 @@ module.exports = {
   env: {
     node: true,
   },
+  plugins: [
+    '@typescript-eslint',
+  ],
   extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:vue/essential',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
   parserOptions: {
+    parser: "@typescript-eslint/parser",
     ecmaVersion: 2020,
   },
   rules: {
